c5ca35fe27630de355dcb4a44dcb32d477d0e3ea
EC2 Default User <ec2-user@ip-172-31-24-127.us-east-2.compute.internal>
2021-09-16 05:58:41 +0000

    finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -16,7 +16,7 @@ def max_2_sum arr
   elsif arr.length == 1
     return arr[0]
   else
-    if arr[0] > arr[1]:
+    if arr[0] > arr[1]
       max_1 = arr[0]
       max_2 = arr[1]
     else
@@ -36,51 +36,75 @@ def max_2_sum arr
 end
 
 def sum_to_n? arr, n
-  if arr.length == 0
+  if arr.length <= 1
     return false
   else
-    arr.each do |ele|
+    arr.length.times do
+      ele = arr.pop(1)[0]
       if arr.include? n-ele
         return true
       end
     end
-    return false 
+    return false
   end
 end
 
 # Part 2
 
 def hello(name)
-  return "hello {#name}"
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
   vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
-  ret_flag = False
+  char_pattern = '[a-zA-Z]'
+  ret_flag = false
   if s[0]
-    if not vowels.include? s[0]
-      ret_flag = True
-    end
+    if s[0].match char_pattern
+      if not vowels.include? s[0]
+        ret_flag = true
+      end
+    end 
   end
   return ret_flag
 end
 
 def binary_multiple_of_4? s
-  pattern = "[1, 0]{4}"
-  return s.match pattern
+  valid_binary_check = true
+  multi4_gt0_pattern = "00$"
+  ret_multiple4 = false
+
+  s.split('').each do |char|
+    if not ['0', '1'].include? char
+      valid_binary_check = false
+    end
+  end
+  
+  if valid_binary_check
+    if s == '0'
+      ret_multiple4 = true
+    elsif s.match multi4_gt0_pattern and s.match '1'
+      ret_multiple4 = true
+    end
+  end
+
+  return ret_multiple4
 end
 
 # Part 3
 
 class BookInStock
-  def initialization isbn_no, price
-    raise ArgumentError, "isbn_no is empty" unless isbn_no.length > 0
-    raise ArgumentError, "price should be lager than zero" unless price >= 0
+  def initialize isbn, price
+    raise ArgumentError, "isbn is empty" unless isbn.length > 0
+    raise ArgumentError, "price should be lager than zero" unless price > 0
     @price = price
-    @isbn_no = isbn_no
-  end 
+    @isbn = isbn
+  end
+  
+  attr_accessor :price
+  attr_accessor :isbn
 
   def price_as_string 
-    puts "$%0.2f" % @price
+    return "$%0.2f" % @price
   end
 end
